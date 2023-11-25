57524ae42439886058bcf90d9e520b89ad1f53fb
hunglnvhe181239 <lenguyenviethung1509@gmail.com>
2023-11-25 09:02:29 +0700

    upload Hello World

---
+++ First Project/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 100;
+        String x = "VCL";
+        System.out.println("Hello World " + a + " " + x); //newline
+        System.out.print("OIVCT");
+        System.out.println();
+        System.out.format("%d laf so", a);
+    }
+}
